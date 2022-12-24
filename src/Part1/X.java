f23fd6532076a3e01a3a8e91306313cecfd33e8e
kubraguler <kubra.topcuoglu@me.com>
2022-12-24 16:31:20 -0500

    Added new line

--- src/Part1/X.java
+++ src/Part1/X.java
@@ -1,10 +1,10 @@
-package X;
+package Part1;
 
 public class X {
     public static void main(String[] args) {
 
 
-        System.out.println("X");
+        System.out.println("Part1");
         System.out.println("Y");
 
 
