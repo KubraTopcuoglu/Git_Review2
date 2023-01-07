6897dc83309ff933a7f3e0a6f59d60cfcb9cfdd8
KubraTopcuoglu <114306547+KubraTopcuoglu@users.noreply.github.com>
2023-01-07 14:32:56 -0500

    Update FirstClass.java

--- src/firsPackage/FirstClass.java
+++ src/firsPackage/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("First Class");
+        //Some changes are made
+        System.out.println("First Class");
 
     }
 
